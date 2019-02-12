eae095a8d0758df2e9cbe4ecd4724f12402aa974
Fabian Großmann <grossmann_fabian@web.de>
2019-02-12 09:06:54 +0100

    Connector angelegt

---
+++ EccCloudWeb/Connector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace EccCloudWeb
+{
+    public class Connector
+    {
+        public int checkUser(string user, char hash[])
+        {
+            //Prüfe ob User in Datenbank enthalten
+
+            //Überprüfe Hash-Wert
+
+            int sessionId = 0;
+            return sessionId;
+        }
+
+        private void connect()
+        {
+
+
+            string myConnectionString = "SERVER=localhost;" +
+                                        "DATABASE=mydatabase;" +
+                                        "UID=user;" +
+                                        "PASSWORD=mypassword;";
+
+            MySqlConnection connection = new MySqlConnection(myConnectionString);
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM mytable";      // @@@ SQLAbfrage Beispiel
+            MySqlDataReader Reader;
+            connection.Open();
+            Reader = command.ExecuteReader();
+            while (Reader.Read())
+            {
+                string row = "";
+                for (int i = 0; i < Reader.FieldCount; i++)
+                    row += Reader.GetValue(i).ToString() + ", ";
+                Console.WriteLine(row);
+            }
+            connection.Close();
+
+
+        }
+    }
+}
